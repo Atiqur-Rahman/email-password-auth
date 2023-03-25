5a094e1bf5e49ccd14678fc169f23fe756093e7d
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-03-25 17:36:27 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,145 @@
-import logo from './logo.svg';
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import app from './firebase.init';
+import { Button, Form } from 'react-bootstrap';
+import { useState } from 'react';
 
+const auth = getAuth(app);
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [validated, setValidated] = useState(false);
+    const [error, setError] = useState('');
+    const [registered, setRegistered] = useState(false);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmailBlur = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePasswordBlur = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleRegisteredChange = (event) => {
+        setRegistered(event.target.checked);
+    };
+
+    const handleNameBlur = (event) => {
+        setName(event.target.value);
+    };
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        const form = event.currentTarget;
+
+        if (form.checkValidity() === false) {
+            event.stopPropagation();
+            return;
+        }
+        if (!/(?=.*?[#?!@$%^&*-])/.test(password)) {
+            setError('Password should contain atleast special character');
+            return;
+        }
+
+        setError('');
+        setValidated(true);
+
+        if (registered) {
+            signInWithEmailAndPassword(auth, email, password)
+                .then((result) => {
+                    const user = result.user;
+                    console.log(user);
+                })
+                .catch((error) => {
+                    console.error(error);
+                    setError(error.message);
+                });
+        } else {
+            createUserWithEmailAndPassword(auth, email, password)
+                .then((result) => {
+                    const user = result.user;
+                    console.log(user);
+                    setEmail('');
+                    setPassword('');
+                    setUserName('');
+                    verifyEmail();
+                })
+                .catch((error) => {
+                    console.error(error);
+                    setError(error.message);
+                });
+        }
+
+        event.preventDefault();
+    };
+
+    const handlePasswordReset = () => {
+        sendPasswordResetEmail(auth, email).then(() => {
+            console.log('email sent');
+        });
+    };
+
+    const setUserName = () => {
+        updateProfile(auth.currentUser, {
+            displayName: name,
+        })
+            .then(() => {
+                console.log('updating name');
+            })
+            .catch((error) => {
+                setError(error.message);
+            });
+    };
+
+    const verifyEmail = () => {
+        sendEmailVerification(auth.currentUser).then(() => {
+            console.log('Email Verification Sent');
+        });
+    };
+
+    return (
+        <div>
+            <div className="registration w-50 mx-auto mt-5">
+                <h2 className="text-primary">Please {registered ? 'Login' : 'Register'}!!</h2>
+                <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+                    {!registered && (
+                        <Form.Group className="mb-3">
+                            <Form.Label>Your Name</Form.Label>
+                            <Form.Control onBlur={handleNameBlur} type="text" placeholder="Your Name" required />
+                            <Form.Control.Feedback type="invalid">Please provide your name.</Form.Control.Feedback>
+                        </Form.Group>
+                    )}
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
+                        <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+                        <Form.Control.Feedback type="invalid">Please provide a valid email.</Form.Control.Feedback>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
+                        <Form.Control.Feedback type="invalid">Please provide a valid password.</Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check onChange={handleRegisteredChange} type="checkbox" label="Already Registered" />
+                    </Form.Group>
+                    <p className="text-danger">
+                        <small>{error}</small>
+                    </p>
+                    <Button onClick={handlePasswordReset} variant="link">
+                        Forget Password?
+                    </Button>
+                    <br />
+                    <Button variant="primary" type="submit">
+                        {registered ? 'Login' : 'Register'}
+                    </Button>
+                </Form>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyC1t3C_4wM4PnRn-uNjQftgDjDFyxsr2PY',
+    authDomain: 'email-password-auth-db0c0.firebaseapp.com',
+    projectId: 'email-password-auth-db0c0',
+    storageBucket: 'email-password-auth-db0c0.appspot.com',
+    messagingSenderId: '294306416434',
+    appId: '1:294306416434:web:3979408c64f7094898392b',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
